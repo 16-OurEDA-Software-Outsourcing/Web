7bc6b52e59e5a9655ac6b0a54c1ef976ad15690c
lcr <1558531230@qq.com>
2017-04-09 18:23:10 +0800

    11

--- app.js
+++ app.js
@@ -4,9 +4,13 @@
 const koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const router = require('./router/route');
-const app = new koa();
 const session = require("koa-session2");
-const Store = require("./redis/Store.js");
+const Store = require("./redis/store.js");
+const res_api = require('koa.res.api');
+const IO = require('koa-socket');
+const app = new koa();
+const io = new IO();
+app.use(res_api());
 app.use(bodyParser());
 app.use(session({
     store: new Store()
@@ -14,10 +18,13 @@ app.use(session({
 app.use(async(ctx,next)=>{
     if(ctx.session.view===undefined) ctx.session.view=0;
     else ctx.session.view++;
-    console.log(ctx.session.view);
+    console.log(ctx.session.user,ctx.session.view);
     await next();
 });
 app
     .use(router.routes())
     .use(router.allowedMethods());
+app.use(async ctx=>{
+    console.log("1");
+});
 app.listen(3000);
--- router/route.js
+++ router/route.js
@@ -7,39 +7,25 @@ const md5 = require('md5');
 const nodemailer = require('nodemailer');
 let users = model.users;
 let transporter = nodemailer.createTransport({
-    service:'126',
+    service: '126',
     auth: {
         user: 'pobooks@126.com',
         pass: 'pobooks126'
     }
 });
-router.post('/action=signUpVerify',async (ctx,next)=>{
-    ctx.verify=ctx.request.body.verify;
-    if(ctx.verify===ctx.custom_signUpVerify){
-        users.create({
-            email:ctx.custom_email.toString(),
-            password:ctx.custom_password.toString()
-        }).then(p=>{
-            console.log(`creat`+JSON.stringify(p));
-        }).catch(err=>{
-            console.log(`failed:`+err);
-            ctx.body=`注册失败`+`err`;
-        });
-    }
-    else ctx.request.status=406
-});
-router.post('/action=signup',async (ctx,next)=>{
-    ctx.custom_email = ctx.request.body.email;
-    ctx.custom_password = ctx.request.body.password;
-    let user = await users.count({where:{email:ctx.custom_email.toString()}});
-    if(user===1) ctx.request.status=406;
+
+router.post('/action=signUpVerify', async (ctx, next) => {
+    ctx.custom_Email = ctx.request.body.email;
+    let n = Math.floor(Math.random() * 9000 + 1000);
+    ctx.session.verify = n.toString();
+    let user = await users.count({where: {email: ctx.custom_Email}});
+    if (user === 1) ctx.response.status = 406;
     else {
-        ctx.custom_signUpVerify=md5(ctx.custom_email);
         let mailOptions = {
             from: '"pobooks" <pobooks@126.com>', // sender address
-            to: ctx.custom_email.toString(), // list of receivers
+            to: ctx.custom_Email, // list of receivers
             subject: '破万卷验证码', // Subject line
-            text: '您的验证码为'+ctx.custom_signUpVerify, // plain text body
+            text: '您的验证码为:' + ctx.session.verify, // plain text body
         };
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
@@ -47,29 +33,79 @@ router.post('/action=signup',async (ctx,next)=>{
             }
             console.log('Message %s sent: %s', info.messageId, info.response);
         });
+        ctx.response.status = 200;
     }
     await next();
 });
-router.post('/action=login',async (ctx,next)=>{
+router.post('/action=signup', async (ctx, next) => {
     ctx.custom_email = ctx.request.body.email;
     ctx.custom_password = ctx.request.body.password;
-    let user = await users.findOne({where:{email:ctx.custom_username.toString()}});
-    if(user.password===ctx.custom_password){
-        let md = md5(ctx.custom_email.toString());
-        ctx.cookies.set(
-            'user',
-            md,
-            {
-                domain: '123.206.71.182',  // 写cookie所在的域名
-                path: '/',       // 写cookie所在的路径
-                maxAge: 60*60*24*1000*365, // cookie有效时长
-                httpOnly: true,  // 是否只用于http请求中获取
-                overwrite: true  // 是否允许重写
-            }
-        );
-        ctx.session.user=ctx.custom_email;
+    ctx.custom_verify = ctx.request.body.verify;
+    if (ctx.session.verify === ctx.custom_verify) {
+        users.create({
+            email: ctx.custom_email,
+            password: ctx.custom_password
+        });
+        ctx.response.status = 200;
     }
-    else ctx.request.status=406;
+    else ctx.response.status = 406;
     await next();
 });
-module.exports=router;
+router.post('/action=login', async (ctx, next) => {
+    ctx.custom_email = ctx.request.body.email;
+    ctx.custom_password = ctx.request.body.password;
+    if (ctx.cookie.get('user', {}) === undefined) {
+        let user = await users.findOne({where: {email: ctx.custom_email}});
+        if (user.password === ctx.custom_password) {
+            let md = md5(ctx.custom_email);
+            ctx.cookies.set(
+                'user',
+                md,
+                {
+                    domain: '123.206.71.182',  // 写cookie所在的域名
+                    path: '/',       // 写cookie所在的路径
+                    maxAge: 60 * 60 * 24 * 1000 * 365, // cookie有效时长
+                    httpOnly: true,  // 是否只用于http请求中获取
+                    overwrite: true  // 是否允许重写
+                }
+            );
+            ctx.session.user = ctx.custom_email;
+            ctx.response.status = 200;
+            let data={
+                "name":"hahahahhaha",
+                "age":"18"
+            };
+            ctx.api(data);
+        }
+        else ctx.response.status = 406;
+    }
+    else ctx.response.status = 200;
+    await next();
+});
+router.post('/action=details', async (ctx, next) => {
+    ctx.custom_user_id = ctx.request.body.user_id;
+    ctx.custom_phoneNum = ctx.request.body.phone;
+    ctx.Secret_question = ctx.request.body.question;
+    ctx.Secret_answer = ctx.request.body.answer;
+    users.create({
+        user_id: ctx.custom_user_id.toString(),
+        phone: ctx.custom_phoneNum.toString(),
+        question: ctx.Secret_question.toString(),
+        answer: ctx.Secret_answer.toString()
+    }).then(p => {
+        console.log(`creat` + JSON.stringify(p));
+    }).catch(err => {
+        console.log(`failed:` + err);
+    });
+    await next();
+});
+router.get('/action=json',async (ctx,next)=>{
+    let data={
+        "name":"hahahahhaha",
+        "age":"18"
+    };
+    ctx.api(data);
+    console.log(ctx.response);
+    await next();
+});
+module.exports = router;
